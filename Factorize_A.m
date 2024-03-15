5d419b5d321b616bc21202f66486408d84d6be87
koriolismsu <163430690+koriolismsu@users.noreply.github.com>
2024-03-15 15:47:10 +0000

    made a loop

--- Factorize_A.m
+++ Factorize_A.m
@@ -1,1 +1,11 @@
 %%%% let us try to factorize matrix A
+
+%% Introduce a loop
+k = 1+0.01i;
+step = 0.01;
+xi_plus_loop = [0:step:1.5,1.5 + 1i*(0:step:0.1),1.5 + 0.1i - (0:step:1.5), 1i*(0.1:-step:0)];
+
+gamma_0 = sqrt(k);
+
+figure;
+plot(xi_plus_loop,'*')
